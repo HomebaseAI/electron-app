9a9d02180ad7ecc0088fca69b5a93a172c326706
Eric Schmar <schmare@ksu.edu>
2018-02-23 12:56:47 -0600

    adding logging ye

--- main.js
+++ main.js
@@ -60,14 +60,15 @@ function createWindow() {
   })
 }
 
-
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', function () {
   createWindow()
-  autoUpdater.checkForUpdates();
+});
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
 });
 
 autoUpdater.on('checking-for-update', () => {
